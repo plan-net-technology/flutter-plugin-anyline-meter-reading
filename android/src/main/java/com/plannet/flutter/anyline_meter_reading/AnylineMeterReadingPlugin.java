df552eae8510537d380d6707311409ff35b47e8e
Gabriel Dusa <g.dusa@plan-net.com>
2020-09-23 19:21:53 +0300

    Add aditional logs to anyline meter reading plugin.

--- android/src/main/java/com/plannet/flutter/anyline_meter_reading/AnylineMeterReadingPlugin.java
+++ android/src/main/java/com/plannet/flutter/anyline_meter_reading/AnylineMeterReadingPlugin.java
@@ -1,56 +1,90 @@
-/*
- * *
- *  * Created by Gabriel Dusa on 6/30/20 11:11 AM
- *  * Copyright (c) 2020 Plan.net Technology . All rights reserved.
- *  * Last modified 6/30/20 11:11 AM
- *
- */
+package com.plannet.flutter.anyline_meter_reading;
 
-package com.plannet.ewi;
+import android.app.Activity;
+import android.content.Intent;
 
-import android.app.Application;
-import android.content.Context;
-import android.content.SharedPreferences;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-import com.adobe.marketing.mobile.*;
-import com.plannet.ewi.*;
+import android.util.Log;
 
-public class AdobeAnalyticsManager {
-  final static int sharedPreferencesMode = Context.MODE_PRIVATE;
-  final  static String sharedPreferencesName = "FlutterSharedPreferences";
-  static final String kTrackingEnabledStatus = "flutter.KEY_TRACKING_STATUS"; // we need 'flutter.' as this is how the Flutter
-  // plugin shared_preferences prefixes the keys
-  static final String TAG = "AdobeAnalyticsManager";
+/** AnylineMeterReadingPlugin */
+public class AnylineMeterReadingPlugin implements MethodCallHandler, PluginRegistry.ActivityResultListener {
+  private final Activity activity;
+  private MethodChannel.Result result;
+  private String licenseKey;
+  private boolean isErrorDueToMethodCall = false;
 
-  static public void setupAnalytics(Application application) {
-    MobileCore.setApplication(application);
-    MobileCore.configureWithAppID(BuildConfig.ADOBE_ANALYTICS_APP_ID);
-    try {
-      Analytics.registerExtension(); //Register Analytics with Mobile Core
-      Identity.registerExtension();
-      MobileCore.setWrapperType(WrapperType.FLUTTER);
-      MobileCore.start(null);
-      Lifecycle.registerExtension();
-    } catch (Exception e) {
-      android.util.Log.e(TAG, "setupAnalytics: " + "exception: " + e.toString());
+  /**
+   * Plugin registration.
+   */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "anyline_meter_reading");
+    final AnylineMeterReadingPlugin instance = new AnylineMeterReadingPlugin(registrar.activity());
+    registrar.addActivityResultListener(instance);
+    channel.setMethodCallHandler(instance);
+  }
+
+  private AnylineMeterReadingPlugin(Activity activity) {
+    this.activity = activity;
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    this.result = result;
+    if (result == null) {
+      isErrorDueToMethodCall = true;
+    }
+    switch (call.method) {
+      case Constants.METHOD_SET_LICENSE_KEY:
+        this.licenseKey = call.argument(Constants.KEY_LICENSE_KEY);
+        result.success("");
+        break;
+      case Constants.METHOD_GET_METER_VALUE:
+        startScanActivity(activity);
+        break;
+      default:
+        result.notImplemented();
+        break;
     }
   }
 
-  static public void lifecycleStart(Application application) {
-    if (isTrackingEnabled(application)) {
-      MobileCore.setApplication(application);
-      MobileCore.lifecycleStart(null);
+  @Override
+  public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+    Log.e("anyline_meter_reading", "onActivityResult");
+    if (requestCode == Constants.SCAN_ACTIVITY_REQUEST_CODE) {
+      if (resultCode == Constants.RESULT_SUCCESS) {
+        if (result != null) {
+          Log.e("anyline_meter_reading", "resultCode == Constants.RESULT_SUCCESS; sent meter reading");
+          result.success(data.getStringExtra(Constants.KEY_METER_VALUE));
+        } else {
+          if (isErrorDueToMethodCall) {
+            Log.e("anyline_meter_reading", "onActivityResult: isErrorDueToMethodCall");
+          } else {
+            Log.e("anyline_meter_reading", "onActivityResult: result is null due to release");
+          }
+          result.error("scan error", null, null);
+        }
+        return true;
+      } else if (resultCode == Constants.RESULT_EXCEPTION_DEFAULT) {
+        result.error(String.valueOf(Constants.RESULT_EXCEPTION_DEFAULT), data.getStringExtra(Constants.KEY_EXCEPTION), null);
+        return true;
+      } else if (resultCode == Constants.RESULT_EXCEPTION_NO_CAMERA_PERMISSION) {
+        result.error(String.valueOf(Constants.RESULT_EXCEPTION_NO_CAMERA_PERMISSION), null, null);
+        return true;
+      }
     }
+    return false;
   }
 
-  static public void lifecyclePause(Application application) {
-    if (isTrackingEnabled(application)) {
-      MobileCore.lifecyclePause();
-    }
+  private void startScanActivity(Activity activity) {
+    Intent i = new Intent(activity, ScanActivity.class);
+    i.putExtra(Constants.KEY_LICENSE_KEY, licenseKey);
+    activity.startActivityForResult(i, Constants.SCAN_ACTIVITY_REQUEST_CODE);
   }
 
-  static private boolean isTrackingEnabled(Application application) {
-    final SharedPreferences sharedPreferences = application.getSharedPreferences(sharedPreferencesName, sharedPreferencesMode);
-    return sharedPreferences.getBoolean(kTrackingEnabledStatus, false);
-  }
 }
